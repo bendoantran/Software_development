3e9bf7759a6b7e0b0b0eddb5063545ecff6f07b5
Canineshoe75 <canineshoe@gmail.com>
2025-04-08 18:12:48 -0400

    Updated activity_main.xml with populating dropdown menu

--- app/src/main/java/com/example/healthtrackprototype/MainActivity.java
+++ app/src/main/java/com/example/healthtrackprototype/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.healthtrackprototype;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner symptoms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        symptoms = findViewById(R.id.symptoms);
+        String[] options = {"Aching Back", "Breathlessness", "Cough", "Diarrhea", "Fatigue", "Headache", "Muscle Cramp", "Nausea"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, options);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        symptoms.setAdapter(adapter);
+
     }
+
 }
